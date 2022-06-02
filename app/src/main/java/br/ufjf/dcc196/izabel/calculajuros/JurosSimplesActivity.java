25d705a7a41aac4bdddf0c789f65e74c6dbd1f49
dduartevla <deboraduarte@ice.ufjf.br>
2022-06-01 22:44:06 -0300

    Retornar o valor

--- app/src/main/java/br/ufjf/dcc196/izabel/calculajuros/JurosSimplesActivity.java
+++ app/src/main/java/br/ufjf/dcc196/izabel/calculajuros/JurosSimplesActivity.java
@@ -2,13 +2,53 @@ package br.ufjf.dcc196.izabel.calculajuros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class JurosSimplesActivity extends AppCompatActivity {
 
+    private TextView textViewValorPresente;
+    private Double valorPresente;
+    private EditText editTextTaxaDeJuros;
+    private EditText editTextPeriodos;
+    private TextView textViewResultado;
+    private Double valorFinal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juros_simples);
+        textViewValorPresente = findViewById(R.id.textViewValePresente);
+        Bundle extras = getIntent().getExtras();
+        valorPresente = extras.getDouble("ValorPresente");
+        textViewValorPresente.setText(valorPresente.toString());
+        editTextTaxaDeJuros = findViewById(R.id.editTextTaxaJuroSimples);
+        editTextPeriodos = findViewById(R.id.editTextPeriodo);
+        textViewResultado = findViewById(R.id.textViewResultado);
+    }
+
+    public void retornaClick (View view){
+
+        Intent resultado = new Intent();
+        resultado.putExtra("valorFinal", valorFinal);
+        setResult(1, resultado);
+        finish();
+    }
+
+    public void calcularClick(View view){
+
+        Double taxaDeJuros;
+        Integer periodos;
+        taxaDeJuros = Double.parseDouble(editTextTaxaDeJuros.getText().toString());
+        periodos = Integer.parseInt(editTextPeriodos.getText().toString());
+
+        valorFinal = valorPresente * (1 + taxaDeJuros*periodos);
+
+        textViewResultado.setText(valorFinal.toString());
+
     }
 }
--- app/src/main/java/br/ufjf/dcc196/izabel/calculajuros/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/izabel/calculajuros/MainActivity.java
@@ -1,14 +1,50 @@
 package br.ufjf.dcc196.izabel.calculajuros;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editTextValorPresente;
+    TextView textViewValorFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextValorPresente = findViewById(R.id.editTextValorPresente);
+
+        textViewValorFinal = findViewById(R.id.textViewValorFinal);
+    }
+
+    public void jurosSimplesClick(View view){
+        try {
+            Double valorPresente = Double.parseDouble(editTextValorPresente.getText().toString());
+            Intent intent = new Intent(MainActivity.this, JurosSimplesActivity.class);
+
+            intent.putExtra("valorPresente", valorPresente);
+            startActivityForResult(intent,1);
+        } catch (Exception e){
+            editTextValorPresente.selectAll();
+            editTextValorPresente.requestFocus();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Double valorFinal = extras.getDouble("valorFinal");
+            textViewValorFinal.setText(valorFinal.toString());
+        }
     }
 }
