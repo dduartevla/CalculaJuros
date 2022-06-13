2ddd9b50d5a926837b1ead00fcce231eca7ca57f
dduartevla <deboraduarte@ice.ufjf.br>
2022-06-13 11:26:29 -0300

    Criação do layout da Activity "ValorFuturo";

--- app/src/main/java/br/ufjf/dcc196/izabel/calculajuros/ValorFuturo.java
+++ app/src/main/java/br/ufjf/dcc196/izabel/calculajuros/ValorFuturo.java
@@ -3,12 +3,34 @@ package br.ufjf.dcc196.izabel.calculajuros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ValorFuturo extends AppCompatActivity {
 
+    private TextView textViewValorPresente;
+    private Double valorPresente;
+    private EditText editTextValorFuturo;
+    private Button buttonCalcular;
+    private TextView textViewPorcentagem;
+    private Button buttonRetornar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valor_futuro);
+        textViewValorPresente = findViewById(R.id.editTextValorPresente);
+        Bundle extras = getIntent().getExtras();
+        valorPresente = extras.getDouble("valorPresente");
+        textViewValorPresente.setText(valorPresente.toString());
+        editTextValorFuturo = findViewById(R.id.editTextValorPresente);
+        buttonCalcular = findViewById(R.id.buttonCalcular);
+        textViewPorcentagem = findViewById(R.id.textViewPorcentagem);
+        buttonRetornar = findViewById(R.id.buttonRetornar);
+    }
+
+    public void calcularClick(View view){
+
     }
 }
